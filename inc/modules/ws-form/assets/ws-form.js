a14745554186d97fb5500547341fa13916c6e1f9
WebDevs.pro <shock_31337@ukr.net>
2023-11-28 07:14:42 +0200

    1.3

--- inc/modules/ws-form/assets/ws-form.js
+++ inc/modules/ws-form/assets/ws-form.js
@@ -10,7 +10,35 @@
 			}
 
 			$( '.cc-signature-preview label.wsf-label' ).text( value );
-		} )
+		} );
+
+
+
+
+
+		$( '.cc-initials-field input' ).on( 'input', function() {
+			var value = $( this ).val();
+			var first_letter = value.length > 0 ? value[0] : 'J';
+			var middle_letter = 'M'; // Default value
+			var last_letter = 'D';   // Default value
+
+			if (value.length == 2) {
+				last_letter = value[1];
+			} else if (value.length >= 3) {
+				middle_letter = value[1];
+				last_letter = value[2];
+			}
+
+			console.log('first_letter', first_letter);
+			console.log('middle_letter', middle_letter);
+			console.log('last_letter', last_letter);
+
+			$( 'text.cc-first-letter' ).text( first_letter );
+			$( 'text.cc-middle-letter' ).text( middle_letter );
+			$( 'text.cc-last-letter' ).text( last_letter );
+		} );
+
+
 
 
 	} );
