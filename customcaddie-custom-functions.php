8e866d1f56578c162780ba2bf0a5c19bcd0a38b1
WebDevs.pro <shock_31337@ukr.net>
2023-11-30 14:49:06 +0200

    1.4.4

--- customcaddie-custom-functions.php
+++ customcaddie-custom-functions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CustomCaddie Custom Functions
  * Plugin URI: https://github.com/webdevs-pro/customcaddie-custom-functions
- * Version: 1.4.3
+ * Version: 1.4.4
  * Description: A place for custom functions for customcaddie.co website
  * Author: Alex Ishchenko
  * Author URI: https://website.cv.ua
