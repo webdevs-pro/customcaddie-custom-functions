3c07cf05122dfc5fc28515c4bf2ba3d6f334c306
WebDevs.pro <shock_31337@ukr.net>
2023-12-04 17:33:55 +0200

    1.6.1

--- inc/modules/ws-form/assets/ws-form.js
+++ inc/modules/ws-form/assets/ws-form.js
@@ -3,7 +3,7 @@
 	// Create wsf-rendered event handler
 	$(document).on('wsf-rendered', function(e, form, form_id, instance_id) {
 
-		$( '.cc-first-name-field, .cc-middle-name-field, .cc-last-name-field' ).on( 'input', function() {
+		function fill_name_placeholders() {
 			var first_name = $( '.cc-first-name-field' ).val();
 			var middle_name = $( '.cc-middle-name-field' ).val();
 			var last_name = $( '.cc-last-name-field' ).val();
@@ -28,7 +28,16 @@
 			full_name += ' ' + last_name;
 	
 			$( '.cc-signature-preview .wsf-tile label.wsf-label' ).text( full_name );
-	} );
+			$( '.cc-preview-wrapper .cc-preview-name' ).text( full_name );
+			$( '.cc-preview-wrapper .cc-preview-signature' ).text( full_name );
+		}
+		fill_name_placeholders();
+
+
+
+		$( '.cc-first-name-field, .cc-middle-name-field, .cc-last-name-field' ).on( 'input', function() {
+			fill_name_placeholders();
+		} );
 
 
 
@@ -122,21 +131,140 @@
 		$('.cc-first-name-field, .cc-middle-name-field, .cc-last-name-field').on('input', function() {
 			// Initialize the initials as an empty string
 			var initials = '';
- 
+
 			// Check each field and add its first letter to the initials if it's not empty
 			if ($('.cc-first-name-field').val()) {
-				 initials += $('.cc-first-name-field').val().charAt(0);
+				initials += $('.cc-first-name-field').val().charAt(0);
 			}
 			if ($('.cc-middle-name-field').val()) {
-				 initials += $('.cc-middle-name-field').val().charAt(0);
+				initials += $('.cc-middle-name-field').val().charAt(0);
 			}
 			if ($('.cc-last-name-field').val()) {
-				 initials += $('.cc-last-name-field').val().charAt(0);
+				initials += $('.cc-last-name-field').val().charAt(0);
 			}
- 
+
 			// Set the initials to the initials field and convert them to uppercase
 			$('.cc-initials-field').val(initials.toUpperCase()).trigger('input');
-	  });
-	} );
+		} );
 
+
+
+
+
+
+
+		function render_ball_preview() {
+			var type = $( '.cc-ball-print-type:checked' ).val();
+			console.log('type', type);
+
+			if ( type == 'Signature Only') {
+				$( '.cc-preview-wrapper .cc-preview-name' ).hide();
+			} else {
+				$( '.cc-preview-wrapper .cc-preview-name' ).show();
+			}
+
+			if ( type == 'Name Only') {
+				$( '.cc-preview-wrapper .cc-preview-signature' ).hide();
+			} else {
+				$( '.cc-preview-wrapper .cc-preview-signature' ).show();
+			}
+		}
+
+		function set_signature_font() {
+
+			var signature_design = $( '.cc-signature-preview input:checked' ).val();
+			console.log('signature_design', signature_design);
+
+
+			switch ( signature_design ) {
+				case 'Dancing Script - Bold 700':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Dancing Script", cursive'
+					} );
+					break;
+
+				case 'Caveat - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Caveat", cursive'
+					} );
+					break;
+
+				case 'Shadows Into Light - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Shadows Into Light", cursive'
+					} );
+					break;
+
+				case 'Satisfy - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Satisfy", cursive'
+					} );
+					break;
+
+				case 'Indie Flower - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Indie Flower", cursive'
+					} );
+					break;
+
+				case 'Great Vibes - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Great Vibes", cursive'
+					} );
+					break;
+
+				case 'Zeyada - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Zeyada", cursive'
+					} );
+					break;
+
+				case 'Sacramento - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Sacramento", cursive'
+					} );
+					break;
+
+				case 'Cookie - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Cookie", cursive'
+					} );
+					break;
+
+				case 'Allura - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Allura", cursive'
+					} );
+					break;
+
+				case 'Alex Brush - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Alex Brush", cursive'
+					} );
+					break;
+
+				case 'Homemade Apple - Regular 400':
+					$( '.cc-preview-wrapper .cc-preview-signature' ).css( {
+						'font-family': '"Homemade Apple", cursive'
+					} );
+					break;
+			}
+		}
+		
+		set_signature_font();
+		render_ball_preview();
+
+		$( '.cc-signature-preview input' ).on( 'change', function() {
+			set_signature_font();
+		} );
+
+		$( '.cc-ball-print-type' ).on( 'change', function() {
+			render_ball_preview();
+		} );
+
+
+
+
+
+	} );
 } )( jQuery );
