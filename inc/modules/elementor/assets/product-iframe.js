af8fcc9a5bd7f128137339cd47f86aee41cbb78d
WebDevs.pro <shock_31337@ukr.net>
2023-12-25 12:18:51 +0200

    1.11

--- inc/modules/elementor/assets/product-iframe.js
+++ inc/modules/elementor/assets/product-iframe.js
@@ -10,6 +10,7 @@ jQuery(window).on('elementor/frontend/init', function () {
          if (window.innerWidth >= 767) {
             // For devices wider than 767px, set height based on content
             iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 5 + 'px';
+            $( iframe.contentWindow.document ).find( 'html' ).css( 'overflow', 'hidden' );
          } else {
             // For devices narrower than 767px, set height to 100vh
             iframe.style.height = '100vh';
@@ -17,7 +18,7 @@ jQuery(window).on('elementor/frontend/init', function () {
       };
 
       // Initialize the iframe resize when it's loaded
-      $( iframe ).on( 'load', function() {
+      $( iframe ).one( 'load', function() {
          resizeIframe();
 
          // Setting up MutationObserver to observe changes in the iframe's content
@@ -27,7 +28,47 @@ jQuery(window).on('elementor/frontend/init', function () {
 
          var config = { attributes: true, childList: true, characterData: true, subtree: true };
          observer.observe( iframe.contentDocument.body, config );
+
+
+
+         var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+         var add_to_cart_button = $( iframeDocument ).find( 'button.single_add_to_cart_button' );
+         var _this = this;
+
+         if ( add_to_cart_button.length > 0 ) {
+            $( add_to_cart_button ).on( 'click', function() {
+               setTimeout( function() {
+                  $( _this.contentDocument.body ).empty();
+
+                  var iframeTop = $( _this ).offset().top;
+                  $( 'html, body' ).animate({
+                      scrollTop: iframeTop - 100
+                  }, 200 );
+               }, 200 );
+
+               $( _this ).on( 'load', function() {
+                  try {
+                     var iframeUrl = $( this ).contents().get( 0 ).location.href;
+                     window.location.href = iframeUrl; 
+                  } catch (error) {
+                     console.error("Error accessing iframe contents:", error);
+                     // Handle errors here (likely due to cross-origin restrictions)
+                  }
+               } );
+            } );
+         }
+
       } );
+      
+      
+
+
+
+
+
+
+
+
    }
 
    elementorFrontend.hooks.addAction( 'frontend/element_ready/cc-product-iframe.default', CCProductIframe );
